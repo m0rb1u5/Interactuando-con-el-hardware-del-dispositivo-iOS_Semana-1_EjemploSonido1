4168915450ce4b40a431c1f2a4f8849cd10cf9f2
m0rb1u5 <goesrex@gmail.com>
2017-04-19 02:53:16 -0500

    Clase "Reproducción de sonido corto".

--- Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-1_EjemploSonido1/ViewController.swift
+++ Interactuando-con-el-hardware-del-dispositivo-iOS_Semana-1_EjemploSonido1/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    private var miSonido: SystemSoundID = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let sonidoURL = Bundle.main.url(forResource: "Submarine", withExtension: "aiff")
+        AudioServicesCreateSystemSoundID(sonidoURL! as CFURL, &miSonido)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func play() {
+        AudioServicesPlaySystemSound(miSonido)
+    }
 }
 
